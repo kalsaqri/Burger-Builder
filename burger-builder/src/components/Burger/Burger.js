c1f656def04342d6f50350051ac403bbe28f4d4e
alsaqrik <kalsaqri@my.ccsu.edu>
2019-02-01 15:34:09 -0500

    Added functionality to Burger Builder so that one can add and remove ingredients and can see pricing. This is included with the css images of the burger

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,30 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'; 
 
 const burger = (props) => {
+    // Object.keys takes keys of Object (e.g. salad) and makes them an array
+    // Array object makes an array of given length (e.g. Array (3) is length 3)
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type = {igKey}/>; 
+            });  
+        })
+        // Reduce reduces array to single value 
+        // Arr is previous value or initial value ([]) and el (current value) to perform function
+        .reduce((arr, el) => { 
+            return arr.concat(el); 
+        }, []);
+
+        console.log(transformedIngredients); 
+
+    if(transformedIngredients.length === 0 ){
+        transformedIngredients = <p>Please start adding Ingredients!</p>
+    }
+    
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
